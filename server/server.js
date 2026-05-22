7b700b27c3cce7d814b20ed46a6665d27ad2ea5e
Abhiram-Bhuvanagiri <abhiii.1818@gmail.com>
2026-05-22 19:34:31 +0530

    Fixed CORS for deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
